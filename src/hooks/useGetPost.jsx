a9ed63be1abea718e17d1b65153923baad992435
Michael <mikkelzuuu@gmail.com>
2022-03-16 16:14:34 +0100

    removed unused imports

--- src/hooks/useGetPost.jsx
+++ src/hooks/useGetPost.jsx
@@ -1,4 +1,4 @@
-import { collection, doc, getDoc, getDocs } from "firebase/firestore"
+import {  doc, getDoc } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { db } from "../firebase"
 
